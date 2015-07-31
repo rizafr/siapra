a841878df58684f355d42917c4a00c05c2657545
ratihpujihati <ratih.pujihati@gmail.com>
2015-07-31 19:40:02 +0700

    format disposisi_cetak.php leuk2 nyaaa

--- application/views/admin/surat_disposisi/disposisi_cetak.php
+++ application/views/admin/surat_disposisi/disposisi_cetak.php
@@ -29,40 +29,50 @@ $q_instansi	= $this->db->query("SELECT * FROM tr_instansi LIMIT 1")->row();
 	</tr>
 	
 	<tr><td colspan="3" align="center" style="padding: 15px 0"><b style="font-size: 21px;">LEMBAR DISPOSISI</b></td></tr>
-	<tr><td width="25%"><b>Indeks Berkas</b></td><td width="50%">: <?php echo $datpil1->indek_berkas; ?></td><td><b>Kode : </b><?php echo $datpil1->kode; ?></td></tr>
-	<tr><td width="25%"><b>Tanggal/Nomor</b></td><td colspan="2">: <?php echo tgl_jam_sql($datpil1->tgl_surat)." / ".$datpil1->no_surat; ?></td></tr>
-	<tr><td><b>Asal Surat</b></td><td colspan="2">: <?php echo $datpil1->dari; ?></td></tr>
-	<tr><td><b>Isi Ringkas</b></td><td colspan="2">: <?php echo $datpil1->isi_ringkas; ?></td></tr>
-	<tr><td><b>Diterima Tanggal</b></td><td colspan="2">: <?php echo tgl_jam_sql($datpil1->tgl_diterima); ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>No. Agenda &nbsp;&nbsp; </b> ( <?php echo $datpil1->no_agenda; ?> ) </td></tr>
-	<tr><td colspan="3"><b>Tanggal Penyelesaian </b>: </td></tr>
-	<tr><td style="height: 350px" valign="top" colspan="2"><b>Isi Disposisi : </b> <br><br>
+	
+	<tr><td></td><td></td>
+	<td  colspan="4"><b align="right">SIFAT SURAT</b>: <?php echo $datpil1->status_surat_masuk; ?></td></tr>
+	
+	<tr><td><b>INDEKS</b></td>
+	<td  width="50%"> : <?php echo $datpil1->index_surat_masuk; ?></td>
+	<td><b align="right">NO. AGENDA</b>
+	: <?php echo $datpil1->no_agenda; ?></td></tr>
+	
+	<tr><td></td><td></td>
+	<td  colspan="4"><b align="right">TGL MASUK</b>: <?php echo $datpil1->tgl_diterima; ?></td></tr>
+</table>
 
+<table>
+	<tr><td><b>PERIHAL</b></td><td colspan="2">: <?php echo $datpil1->perihal_surat_masuk; ?></td></tr>
+	<tr><td width="25%"><b>TGL SURAT</b></td><td colspan="2">: <?php echo tgl_jam_sql($datpil1->tgl_surat_masuk) ?></td></tr>
+	<tr><td width="25%"><b>NO SURAT</b></td><td colspan="2">: <?php echo $datpil1->no_surat_masuk; ?></td></tr>
+	<tr><td><b>ASAL SURAT</b></td><td colspan="2">: <?php echo $datpil1->asal_surat_masuk; ?></td></tr>
+</table>
+
+<table>
+	<tr><td style="height: 350px" valign="top" colspan="2" align="center"><b>INSTRUKSI /  INFORMASI: </b> <br><br>
 	<ol>
 	<?php 
 	if (!empty($datpil3)) {
 		foreach ($datpil3 as $d3) {
-			echo "<li><b><i>".$d3->isi_disposisi."</i></b>. Batas : ".tgl_jam_sql($d3->batas_waktu).", Sifat: ".$d3->sifat." </li>";
+			echo "<li><b><i>".$d3->isi_instruksi."</i>";
 		}
 	}
 	?>
 	</ol>
+	</b></td>
 	
-	
-	</b></td><td valign="top" width="50%" style="border-left: solid 1px">
-	Diteruskan kepada  : 
+	<td  align="center" valign="top" width="50%" style="border-left: solid 1px">
+	<b>DITERUSKAN KEPADA :</b> 
 	<ol>
 	<?php 
 	if (!empty($datpil2)) {
 		foreach ($datpil2 as $dp) {
-			echo "<li>".$dp->kpd_yth."</li>";
+			echo "<li>".$dp->tujuan_disposisi."</li>";
 		}
 	}
 	?>
 	</ol>
-	</td></tr>
-	<tr><td colspan="3" style="line-height: 30px">Sesudah digunakan harap dikembalikan<br>
-	Kepada : ........................................................................................................................................<br>
-	Tanggal : ........................................................................................................................................<br>
 	</td></tr>
 </table>
 </body>
